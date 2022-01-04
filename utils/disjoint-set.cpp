eb5c48c0ea0e9c5d0fbfd7bab180d10b2f533fe6
NaraS91 <39410439+NaraS91@users.noreply.github.com>
2022-01-04 21:38:44 +0100

    feat(disjoint-set): added size method and destructor to fix memory leaks

--- utils/disjoint-set.cpp
+++ utils/disjoint-set.cpp
@@ -1,46 +1,62 @@
 #include "disjoint-set.hpp"
 
-template <typename T>
-Set<T>::Set(T elem){
-  _representative = new Node<T>;
-  _representative->elem = elem;
-  _representative->parent = NULL;
-  _representative->rank = 0;
-}
-
-template <typename T>
-Node<T> *Set<T>::Find(){
-  Node<T> *representative = _representative;
-  while(representative->parent != NULL) {
-    representative = representative->parent;
+namespace utils {
+  template <typename T>
+  Set<T>::Set(T elem){
+    _representative = new Node<T>;
+    _representative->size = 1;
+    _representative->elem = elem;
+    _representative->parent = NULL;
+    _representative->rank = 0;
   }
 
-  Node<T> *child = _representative;
-  while(child->parent != NULL){
-    auto next = child->parent;
-    child->parent = representative;
-    child = next;
+  template <typename T>
+  Set<T>::~Set(){
+    delete _representative;
   }
 
-  return representative;
-}
+  template <typename T>
+  Node<T> *Set<T>::Find(){
+    Node<T> *representative = _representative;
+    while(representative->parent != NULL) {
+      representative = representative->parent;
+    }
 
-template <typename T>
-void Set<T>::Union(Set other){
-  Node<T> *x = Find();
-  Node<T> *y = other.Find();
+    Node<T> *child = _representative;
+    while(child->parent != NULL){
+      auto next = child->parent;
+      child->parent = representative;
+      child = next;
+    }
 
-  if (x == y)
-    return;
-
-  if (x->rank > y->rank) {
-    Node<T> *z = x;
-    x = y;
-    y = z;
+    return representative;
   }
 
-  x->parent = y;
+  template <typename T>
+  void Set<T>::Union(Set other){
+    Node<T> *x = Find();
+    Node<T> *y = other.Find();
 
-  if (x->rank == y->rank)
-    y->rank++;
+    if (x == y)
+      return;
+
+    if (x->rank > y->rank) {
+      Node<T> *z = x;
+      x = y;
+      y = z;
+    }
+
+    x->parent = y;
+
+    y->size += x->size;
+
+    if (x->rank == y->rank)
+      y->rank++;
+  }
+
+  template <typename T>
+  size_t Set<T>::Size(){
+    Node<T> *rep = Find();
+    return rep->size;
+  }
 }
--- utils/disjoint-set.hpp
+++ utils/disjoint-set.hpp
@@ -2,23 +2,27 @@
 #define DISJOINT_SET_CPP
 #include <vector>
 
+namespace utils {
+  template<typename T>
+  struct Node{
+    T elem;
+    size_t rank;
+    size_t size;
+    Node *parent;
+  };
 
-template<typename T>
-struct Node{
-  T elem;
-  size_t rank;
-  Node *parent;
-};
+  template <typename T>
+  class Set{
+    public:
+      Set(T elem);
+      ~Set();
+      Node<T> *Find();
+      void Union(Set other);
+      size_t Size();
 
-template <typename T>
-class Set{
-  public:
-    Set(T elem);
-    Node<T> *Find();
-    void Union(Set other);
-
-  private:
-    Node<T> *_representative;
-};
+    private:
+      Node<T> *_representative;
+  };
+}
 
 #endif
